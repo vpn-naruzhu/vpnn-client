amnezia-vpn/amnezia-client

Change trade marks.

client/platforms/windows/daemon/windowsdaemon.h
@@ -11,7 +11,7 @@
 #include "windowstunnelservice.h"
 #include "wireguardutilswindows.h"
 
-#define TUNNEL_SERVICE_NAME L"WireGuardTunnel$AmneziaVPN"
+#define TUNNEL_SERVICE_NAME L"WireGuardTunnel$VPNNaruzhu"
 
 class WindowsDaemon final : public Daemon {
   Q_DISABLE_COPY_MOVE(WindowsDaemon)

client/platforms/windows/daemon/windowstunnelservice.cpp
@@ -17,7 +17,7 @@
 
 #define TUNNEL_NAMED_PIPE \
   "\\\\."                 \
-  "\\pipe\\ProtectedPrefix\\Administrators\\WireGuard\\AmneziaVPN"
+  "\\pipe\\ProtectedPrefix\\Administrators\\WireGuard\\VPNNaruzhu"
 
 constexpr uint32_t WINDOWS_TUNNEL_MONITOR_TIMEOUT_MSEC = 2000;
 

vpn-naruzhu/vpnn-client

client/platforms/windows/daemon/windowsdaemon.h
@@ -11,7 +11,7 @@
 #include "windowstunnelservice.h"
 #include "wireguardutilswindows.h"
 
-#define TUNNEL_SERVICE_NAME L"AmneziaWGTunnel$AmneziaVPN"
+#define TUNNEL_SERVICE_NAME L"AmneziaWGTunnel$VPNNaruzhu"
 
 class WindowsDaemon final : public Daemon {
   Q_DISABLE_COPY_MOVE(WindowsDaemon)

client/platforms/windows/daemon/windowstunnelservice.cpp
@@ -17,7 +17,7 @@
 
 #define TUNNEL_NAMED_PIPE \
   "\\\\."                 \
-  "\\pipe\\ProtectedPrefix\\Administrators\\AmneziaWG\\AmneziaVPN"
+  "\\pipe\\ProtectedPrefix\\Administrators\\AmneziaWG\\VPNNaruzhu"
 
 constexpr uint32_t WINDOWS_TUNNEL_MONITOR_TIMEOUT_MSEC = 2000;
 